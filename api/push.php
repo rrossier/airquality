4ba4489841a7e7ec4e076d1cf4a8474d88dd9696
Romain Rossier <51224106+rrossier@users.noreply.github.com>
2020-10-04 16:57:26 +0200

    cleaned api endpoint

--- api/push.php
+++ api/push.php
@@ -4,20 +4,18 @@ header("Access-Control-Allow-Methods: *");
 header("Content-Type: application/json");
 
 if($_SERVER['REQUEST_METHOD'] === 'POST'){
-	$key = "1f7e6858-ff54-4190-a994-b477056818fa";
+	$key = "";
 	$targetJsonFile = './data.json';
 
 	if(isset($_SERVER['HTTP_X_KEY'])){
 		$key_sent = $_SERVER['HTTP_X_KEY'];
 		if($key_sent == $key){
 			$data = json_decode(file_get_contents('php://input'), true);
-			//*
 			$existingData = file_get_contents($targetJsonFile);
 			$tempArray = json_decode($existingData);
 			array_push($tempArray, $data['values']);
 			$jsonData = json_encode($tempArray);
 			file_put_contents($targetJsonFile, $jsonData);
-			//*/
 			$results = array('success'=>TRUE);
 		}
 		else{
@@ -31,7 +29,6 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
 else{
 	$results = array('success'=>FALSE, 'error'=>'invalid method');
 }
-
 	// answer
 	echo json_encode($results);
 ?>
